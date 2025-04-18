f184bdb416351c601a8df054b4d10babee68f6be
Aditya Bachina <aditya.bachina@gmail.com>
2025-04-18 13:42:19 -0400

    test

--- data/sample_train_data.py
+++ data/sample_train_data.py
@@ -32,7 +32,7 @@ def main():
         with open(dest_path, 'w') as f:
             json.dump(dest, f, indent=4)
     elif method == "direct":
-        number = 10
+        number = 100
         source_path = f"advbench/harmful_behaviors.csv"
         data = pd.read_csv(source_path)  
         dest_csv_path = f"advbench/harmful_behaviors_{method}.csv"
@@ -48,28 +48,29 @@ def main():
         with open(dest_path, 'w') as f:
             json.dump(dest, f, indent=4)
     elif method == "transfer":
-        test_number = 10
+        test_number = 100
         source_path = f"advbench/harmful_behaviors.csv"
         data = pd.read_csv(source_path) 
         dest_csv_path = f"advbench/harmful_behaviors_{method}.csv"
-        train_dest_path = f"advbench/blank_transfer_train_25.json"
+        #train_dest_path = f"advbench/blank_transfer_train_25.json"
+        train_dest_path = f"advbench/blank_transfer_train_1.json"
         test_dest_path = f"advbench/blank_transfer_test_{test_number}.json"
-        train_goals = data['goal'].tolist()[:25]
+        train_goals = data['goal'].tolist()[:1]
         train_dest = {}
         for goal in train_goals:
             train_dest[goal] = []
         with open(train_dest_path, 'w') as f:
             json.dump(train_dest, f, indent=4)
         test_dest = {}
-        test_indices = random.sample(range(0, len(data['goal'])), test_number) #AB: turn back to 25 instead of 1
+        test_indices = random.sample(range(1, len(data['goal'])), test_number) #AB: turn back to 25 instead of 1
         all_goals = data['goal'].tolist()
         test_goals = [all_goals[i] for i in test_indices]
         for goal in test_goals:
             test_dest[goal] = []
         with open(test_dest_path, 'w') as f:
             json.dump(test_dest, f, indent=4)    
-        #for i in range(25):
-            #test_indices.append(i)
+        for i in range(1):
+            test_indices.append(i)
         test_indices.sort()
         dest_data = data.iloc[test_indices]
         dest_data.to_csv(dest_csv_path, index=False,sep=',')
